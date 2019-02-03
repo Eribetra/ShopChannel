95517fae3308fc5da1041399e56ebe6d341d79d2
Eribetra <ericlaube.dutra@gmail.com>
2019-02-03 16:02:05 -0200

    updated to dark theme
    look at the title

--- public_html/explore.php
+++ public_html/explore.php
@@ -1,7 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
     <title>Explore</title>
     <style type="text/css">
         .heading {
@@ -10,7 +8,7 @@
             font-style: normal;
             font-variant: normal;
             text-transform: none;
-            color: #000000;
+            color: #FFFFFF;
             text-align:center
         }
         .highlighted {
@@ -20,7 +18,7 @@
             font-variant: normal;
             text-transform: none;
             color: #FFFFFF;
-            background-color: #34BEED;
+            background-color: #EDA734;
         }
 
         .default {
@@ -64,11 +62,10 @@
         function FP_goToURL(url) {//v1.0
             window.location=url;
         }
-        shop.setWallpaper(2);
         // -->
     </script>
 </head>
-<body onload="FP_preloadImgs(/*url*/'asset/button/open2.png',/*url*/'asset/button/open3.png',/*url*/'asset/section/clubspotB.png',/*url*/'asset/section/clubspotC.png',/*url*/'asset/section/patchesB.png',/*url*/'asset/section/patchesC.png',/*url*/'asset/section/homebrewB.png',/*url*/'asset/section/homebrewC.png',/*url*/'asset/button/returnB.png',/*url*/'asset/button/returnC.png')">
+<body onload="FP_preloadImgs(/*url*/'asset/button/open2.png',/*url*/'asset/button/open3.png',/*url*/'asset/section/clubspotB.png',/*url*/'asset/section/clubspotC.png',/*url*/'asset/section/patchesB.png',/*url*/'asset/section/patchesC.png',/*url*/'asset/section/homebrewB.png',/*url*/'asset/section/homebrewC.png',/*url*/'asset/button/returnB.png',/*url*/'asset/button/returnC.png')" style="background-color:#252525">
 
 <p class="heading"><img src="asset/logo/bluelogo.png" width="32" height="32"> Open Shop Channel <span class="highlighted">Explore</span>
     <span class="default">&nbsp;&nbsp;&nbsp;&nbsp; </span>
@@ -80,9 +77,9 @@
     <img alt="Club Spot" id="clubspotbut" dir="ltr" height="100" src="asset/section/clubspotA.png" width="100" onmousedown="FP_swapImg(1,1,/*id*/'clubspotbut',/*url*/'asset/section/clubspotC.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'clubspotbut',/*url*/'asset/section/clubspotB.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL('clubspot/index.php')">
     <img alt="Patches" id="patchesbut" dir="ltr" height="100" src="asset/section/patchesA.png" width="100" onmousedown="FP_swapImg(1,1,/*id*/'patchesbut',/*url*/'asset/section/patchesC.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'patchesbut',/*url*/'asset/section/patchesB.png');" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL('patches/list.php');">
     <img alt="Homebrew" id="homebrewbut" dir="ltr" height="100" src="asset/section/homebrewA.png" width="100" onmousedown="FP_swapImg(1,1,/*id*/'homebrewbut',/*url*/'asset/section/homebrewC.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'homebrewbut',/*url*/'asset/section/homebrewB.png');" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL(/*href*/'homebrew/list.php');">
-    </img></p>
+    </p>
 <p class="heading">
     <img alt="Return Button" id="returnbut" dir="ltr" height="69" src="asset/button/return1A.png" width="319" onmousedown="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'asset/button/return1C.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'asset/button/return1B.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL(/*href*/'index.php')">
 </p>
-</body>
-</html>
+
+</body></html>
